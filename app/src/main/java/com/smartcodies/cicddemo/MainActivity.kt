cd28cb7d850a8db4876aa780a20934ffc34e05ac
kishanmorker <kishan.morker@gmail.com>
2023-04-27 16:58:04 +0530

    Initial commit

---
+++ app/src/main/java/com/smartcodies/cicddemo/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.smartcodies.cicddemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.smartcodies.cicddemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+//        throw RuntimeException("Test Crash abc123") // Force a crash
+
+        mBinding.tvText.text = "@${mBinding.tvText.text}" + if (BuildConfig.DEBUG)
+            "Build Type: DEBUG" else "Build Type: Release"
+
+    }
+}
